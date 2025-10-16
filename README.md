6ad75dcb613d8809f0ea059293d6b3047abaa752
Anbarasan <49087852+anbarasananbu@users.noreply.github.com>
2025-10-16 18:05:02 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,123 @@
+# Graphite, Graphene and Graphene Oxide Builder
+
+MakeGraphitics is a library to ceate various atomistic graphitic structures for molecular dynamics.
+
+Available structures:
+- Hexagonal graphene flake
+- Rectangular graphene flake
+- Rectangular perodic graphene sheet (no edges)
+- Periodic graphite 
+- Graphene and graphite oxide
+
+Output:
+- .xyz 
+- lammps data file
+
+Automatically parameterise by forcefields:
+- OPLS
+- GraFF
+- ReaxFF
+
+## Install
+
+Clone this repository. Install using Python2.7. Run the tests to check the installation has worked.
+```
+git clone https://github.com/velocirobbie/make-graphitics
+cd make-graphitics
+python setup.py install
+pytest
+```
+
+A conda environment is provided if you do not have the right packages. If you have conda set up, execute these commands to create a working python environment before the install setp.
+```
+conda env create --file graphene-env.yml
+conda activate graphene
+```
+
+Running `pytest` will create a bunch of unwanted output files. Sorry about this, I will try and tidy up the outputs soon. In the mean time you can remove with `rm *xyz *data`.
+
+## Examples
+
+See the scripts in the `examples/` directory for a number of sample structures.
+
+1) Make a rectangular graphene sheet that extends through periodic boundaries. Parameterised with OPLS and outputs to .xyz for easy veiwing with VMD and a LAMMPS data file.
+```
+python2.7 graphene_sheet.py
+```
+Size of the sheet can be specified in `graphene_sheet.py`.
+
+2) Make a hexagonal flake of graphene oxide. Parameterised with OPLS and outputs to .xyz for easy veiwing with VMD and a LAMMPS data file.
+```
+python2.7 GO_flake.py
+```
+There are several tunable parameters in `GO_flake.py` that you may be interested in. Including:
+- flake radius
+- C/O target ratio, `ratio`
+- Rate at which new nodes are added, `new_island_freq`
+- output snapshots of the oxidation process every N steps with `video_xyz=N`. Viewed in VMD with `topo readvarxyz out.xyz`
+
+## Notes on the Oxidiser 
+
+The Oxidiser takes a graphitic structure and attempts to oxidise it by the process described in (Yang, Angewandte Chemie, 2014; Sinclair, 2019). The algorithm proceeds as follows:
+
+1) If hydrogens exist (i.e. edge of a flake), 1/4 are changed to alcohol groups and 1/4 to carboxyl groups (Lerf and Klinowski model). These values can be changed by passing the Oxidiser object the optional arguments: ` edge_OHratio = 0.25, edge_carboxyl_ratio = 0.5`.
+
+2) The reactivity of every possible site is calculated. This is done by using a ranodom forest approach to extend the data set of GO reactivites given by Yang et al. We do not take into account the reactivity of the edges.
+
+3) A site is oxidised at random weighted by each site's reactivity. The chance of an oxidisation producing an alcohol or epoxy group on the surface is by default 50:50, but can be specified by passing Oxidiser the optional argument: `surface_OHratio = 0.5`
+
+4) The time elapsed between oxidations is estimated from the reactivity of the site that has been oxidised.
+
+5) New nodes are added proportionally to `time_elapsed * new_island_freq`. Note this can be 0. The reasons for doing this are outlined in (Sinclair 2019).
+
+6) Steps 2-5 are repeated until the target C/O ratio is reached or no new sites are available, usually C/O ~ 1.7 . We recommend setting the target, `ratio`, to over 2 as this is what is seen experimentally.
+
+## Notes on Parameterisation
+
+Not all the bonded interactions that can occur in graphene oxide are included in the OPLS parameterisation. We make some neccesary like for like atom-type substitutions to get around this problem. It is not ideal but common practice in molecular dynamics. The substitutions used are outputed after a parameterisation step. Each substitution line outputs the origional atom types, the atom types used to parameterise them, and a summary string that you can use to find in the script `makegraphitics/params.py`. Substitutions keep atom types as close to the origional as possible e.g. replaces an aromatic C with an alkene C, whcih are both sp2 carbon atoms. 
+
+## More structure examples
+
+More examples of building structures with this script are in the `examples` directory.
+
+Note that differenct structures can be combined into one simulation object with `Combine`. Also coordinates can be manipulated before writing to a lammps file. An examploe of this is shown in `peel_sim.py`.
+
+# Citing
+
+The work contained here has been published in some of my own papers e.g.
+
+ - Graphene–Graphene Interactions: Friction, Superlubricity, and Exfoliation https://doi.org/10.1002/adma.201705791
+
+ - Modeling Nanostructure in Graphene Oxide: Inhomogeneity and the Percolation Threshold https://doi.org/10.1021/acs.jcim.9b00114
+
+ - The Role of Graphene in Enhancing the Material Properties of Thermosetting Polymers https://doi.org/10.1002/adts.201800168
+
+I would appreciate a citation if you any of the code in any published work :) You could cite the graphene oxide structure paper, this github page (if the journal allows), or the latest release on the zenodo repository
+
+```
+@article{sinclair2019modelling,
+  title={Modelling nanostructure in graphene oxide: inhomogeneity and the percolation threshold},
+  author={Sinclair, Robert Callum and Coveney, Peter Vivian},
+  journal = {Journal of Chemical Information and Modeling},
+  volume = {59},
+  number = {6},
+  pages = {2741-2745},
+  year = {2019},
+  doi = {10.1021/acs.jcim.9b00114},
+}
+@misc{make-graphitics-github,
+    url = {https://github.com/velocirobbie/make-graphitics},
+    howpublished = {\url{https://github.com/velocirobbie/make-graphitics}},
+    note = {Accessed: \today},
+    author = {Sinclair, Robert C.},
+    year = {2019}
+}
+@misc{make-graphitics_zenodo,
+  author    = {Sinclair, Robert. C. },
+  title     = {make-graphitics},
+  version   = {0.1.0},
+  publisher = {Zenodo},
+  year      = {2019},
+  doi       = {10.5281/zenodo.2548538}
+}
+
---
+++ analysis/islands.py
@@ -0,0 +1,285 @@
+import numpy as np
+from math import pi
+
+
+class Island(object):
+    def __init__(self):
+        self.atoms = []
+        self.coords = []
+        self.area_boundary = None
+        self.area_simple = None
+
+    def populate_coords(self, all_coords):
+        N = len(self.atoms)
+        self.coords = np.empty((N, 3))
+        for i, atom in enumerate(self.atoms):
+            self.coords[i] = all_coords[atom - 1]
+
+    def calc_boundary(self, alpha):
+        self.boundary = outer_polygon(self.coords[:, 0:2], alpha)
+
+    def calc_area(self):
+        self.area_boundary = self.boundary.area
+        self.area_simple = simple_area(self.coords)
+
+    def com(self):
+        return self.coords[:, 0:2].mean(0)
+
+    def natoms(self):
+        return len(self.atoms)
+
+    def diameter(self, method):
+        return 2 * np.sqrt(getattr(self, method) / pi)
+
+
+def build_bond_network(bonds, atom_types):
+    N = len(atom_types)
+    bond_network = {
+        i + 1: {"type": type_, "bonded_to": []} for i, type_ in enumerate(atom_types)
+    }
+    for bond in bonds:
+        bond_network[bond[0]]["bonded_to"] += [bond[1]]
+        bond_network[bond[1]]["bonded_to"] += [bond[0]]
+    return bond_network
+
+
+def flood_island(
+    index, bond_network, island_labels, atom_types, island_index, coords, x, y
+):
+    island = Island()
+
+    def unwrap_coord(coord, ref):
+        dx = coord[0] - ref[0]
+        while (dx > x / 2) or (dx < -x / 2):
+            if dx > x / 2:
+                coord[0] -= x
+                dx -= x
+            elif dx < -x / 2:
+                coord[0] += x
+                dx += x
+        dy = coord[1] - ref[1]
+        while (dy > y / 2) or (dy < -y / 2):
+            if dy > y / 2:
+                coord[1] -= y
+                dy -= y
+            elif dy < -y / 2:
+                coord[1] += y
+                dy += y
+        return coord
+
+    x_min = 0
+    x_max = 0
+    y_min = 0
+    y_max = 0
+
+    def check_island_range(coord, x_min, x_max, y_min, y_max):
+        if coord[0] < x_min:
+            x_min = coord[0]
+        if coord[0] > x_max:
+            x_max = coord[0]
+        if coord[1] < y_min:
+            y_min = coord[1]
+        if coord[1] > y_min:
+            y_max = coord[1]
+        check = True
+        if (x_max - x_min > 5 * x) or (y_max - y_min > 5 * y):
+            check = False
+        return check
+
+    q = [index + 1]
+    refs = [coords.mean(0)]
+    check = True
+    while q:
+        v = q.pop()  # pop removes and returns last element of array
+        ref = refs.pop()
+        island_labels[v - 1] = island_index
+        island.atoms += [v]
+        atom_coord = unwrap_coord(coords[v - 1], ref)
+        check = check_island_range(atom_coord, x_min, x_max, y_min, y_max)
+        if not check:
+            raise Exception("GO is below percolation threshold")
+        island.coords += [atom_coord]
+        neighbours = bond_network[v]["bonded_to"]
+
+        for neighbour in neighbours:
+            atom_type = atom_types[neighbour - 1]
+            already_included = island_labels[neighbour - 1]
+            if (atom_type == 1) and (not already_included):
+                q.append(neighbour)
+                refs.append(atom_coord)
+    island.is_an_island = check
+    return island, island_labels
+
+
+def find_islands_by_flood(sim):
+    bonds = sim.bonds  # 2xN array
+    atom_types = sim.atom_labels
+
+    x, y = [
+        sim.box_dimensions[0, 1] - sim.box_dimensions[0, 0],
+        sim.box_dimensions[1, 1] - sim.box_dimensions[1, 0],
+    ]
+
+    N = len(atom_types)
+    bond_network = build_bond_network(bonds, atom_types)
+    islands = []
+
+    # array recording if atoms are in an island
+    # 0=not an island (graphene); 1,2,3.. = in island N
+    island_labels = np.zeros(N)
+
+    for i in range(N):
+        # if not already counted in an island, and a aromatic carbon
+        if (island_labels[i] == 0) and (atom_types[i] == 1):
+            # found new island
+            island_index = len(islands) + 1
+            island, island_labels = flood_island(
+                i,
+                bond_network,
+                island_labels,
+                atom_types,
+                island_index,
+                sim.coords,
+                x,
+                y,
+            )
+            islands += [island]
+
+    map(lambda island: island.populate_coords(sim.coords), islands)
+
+    return islands
+
+
+def outer_polygon(coords, alpha):
+    if len(coords) < 3:
+        return 0
+    from scipy.spatial import Delaunay
+    from shapely.ops import cascaded_union, polygonize
+    import shapely.geometry as geometry
+
+    def alpha_shape(coords, alpha):
+        def add_edge(edges, edge_points, coords, i, j):
+            """
+            Add a line between the i-th and j-th points,
+            if not in the list already
+            """
+            if (i, j) in edges or (j, i) in edges:
+                # already added
+                return
+            edges.add((i, j))
+            edge_points.append(coords[[i, j]])
+
+        tri = Delaunay(coords)
+        edges = set()
+        edge_points = []
+        # loop over triangles:
+        # ia, ib, ic = indices of corner points of the
+        # triangle
+        for ia, ib, ic in tri.vertices:
+            pa = coords[ia]
+            pb = coords[ib]
+            pc = coords[ic]
+            # Lengths of sides of triangle
+            a = np.sqrt((pa[0] - pb[0]) ** 2 + (pa[1] - pb[1]) ** 2)
+            b = np.sqrt((pb[0] - pc[0]) ** 2 + (pb[1] - pc[1]) ** 2)
+            c = np.sqrt((pc[0] - pa[0]) ** 2 + (pc[1] - pa[1]) ** 2)
+            if a > alpha or b > alpha or c > alpha:
+                pass
+            else:
+                add_edge(edges, edge_points, coords, ia, ib)
+                add_edge(edges, edge_points, coords, ib, ic)
+                add_edge(edges, edge_points, coords, ic, ia)
+        m = geometry.MultiLineString(edge_points)
+        triangles = list(polygonize(m))
+        return cascaded_union(triangles), edge_points
+
+    if len(coords) < 4:
+        # When you have a triangle, there is no sense
+        # in computing an alpha shape.
+        return geometry.MultiPoint(list(coords)).convex_hull
+    else:
+        concave_hull, edge_points = alpha_shape(coords, alpha=alpha)
+        return concave_hull
+
+
+def simple_area(coords):
+    atom_area = (
+        1.414 ** 2 * 3 * np.sqrt(3) / 4
+    )  # 2.60, number density of graphene atoms
+    return len(coords) * atom_area
+
+
+def strip_small_islands(islands, min_atoms):
+    # min_atmos in island to be worth counting
+    new_islands = []
+    for island in islands:
+        if island.natoms() >= min_atoms:
+            new_islands += [island]
+    return new_islands
+
+
+def write_islands_xyz(islands):
+    N = sum([island.natoms() for island in islands])
+
+    with open("islands.xyz", "w") as f:
+        f.write(str(N) + "\n")
+        f.write("islands\n")
+        for i, island in enumerate(islands):
+            for coord in island.coords:
+                f.write(str((i + 1) % 10) + "\t")
+                for axis in coord:
+                    f.write(str(axis) + "\t")
+                f.write("\n")
+
+
+def write_gnuplot(islands):
+    # write object file for polygons
+    with open("island_objects.sh", "w") as f:
+        for i, island in enumerate(islands):
+            if not island.is_an_island:
+                break
+            f.write("set object " + str(i + 1) + " polygon from \\\n")
+            coords = list(island.boundary.exterior.coords)
+            for point in coords:
+                f.write("\t" + str(point[0]) + "," + str(point[1]) + " to \\\n")
+            f.write("\t" + str(coords[-1][0]) + "," + str(coords[-1][1]) + " \n")
+            f.write(
+                "set object "
+                + str(i + 1)
+                + " fc rgb '#000000' fillstyle solid lw 0\n\n"
+            )
+    # write coords of all island atoms
+    with open("island_coords.dat", "w") as f:
+        for island in islands:
+            for coord in island.coords:
+                f.write(str(coord[0]) + "\t " + str(coord[1]) + "\n")
+    # draw circles of an island's approximate area
+    with open("island_area_circle.dat", "w") as f:
+        for island in islands:
+            f.write(
+                str(island.com()[0])
+                + "\t "
+                + str(island.com()[1])
+                + "\t "
+                + str(island.diameter("area_simple") / 2)
+                + "\t"
+                + str(island.diameter("area_boundary") / 2)
+                + "\n"
+            )
+    # plot with:
+    # gnuplot> load 'island_objects.sh'
+    # gnuplot> pl 'island_coords.dat' ,'island_area_circle.dat' w circ
+
+
+def calc_island_sizes(sim):
+    islands = find_islands_by_flood(sim)
+    islands = strip_small_islands(islands, 6)
+
+    map(lambda island: island.calc_boundary(3), islands)
+    map(lambda island: island.calc_area(), islands)
+    write_islands_xyz(islands)
+    write_gnuplot(islands)
+
+    sizes_b = [island.diameter("area_boundary") for island in islands]
+    sizes_s = [island.diameter("area_simple") for island in islands]
+    return sizes_b, sizes_s
